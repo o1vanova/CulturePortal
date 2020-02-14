05fd6fe37fb93d43016b4f6fe683b80f5ebdae26
Olga Ivanova <olgai1409@gmail.com>
2020-02-14 18:37:19 +0500

    feat: add configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": "off",
+    "@typescript-eslint/no-explicit-any": 0,
+  },
+  "settings": {
+    "import/resolver": "webpack",
+    "react": {
+      "version": "detect",
+    },
+  },
+};
