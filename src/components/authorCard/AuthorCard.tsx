4fccaf109a55395eb6a8585c7713af477a18d965
ildar107 <ildar107wk@gmail.com>
2020-02-18 12:54:33 +0300

    refactor: change design for styleguide

--- src/components/authorCard/AuthorCard.tsx
+++ src/components/authorCard/AuthorCard.tsx
@@ -11,7 +11,7 @@ interface Author {
 const AuthorCard = (props:any) => {
   return (
     <a href={'authors/' + props.author.id} className="Author-reference">
-      <Card className="Authors-card">
+      <Card className="Authors-card mb-3">
         <Card.Img className="Authors-card__photo" variant="top" src={props.author.imgSrc}/>
         <Card.Body>
           <Card.Title>{props.author.title}</Card.Title>
