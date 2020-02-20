ef81fcba415a8a141c3b93a4d61ea81bd39e4a5c
DmitryAstapenko <dmitryastapenko1@gmail.com>
2020-02-20 16:51:29 +0300

    fix: changed component BlockArchitectDay

--- src/components/blockArchitectDay/BlockArchitectDay.tsx
+++ src/components/blockArchitectDay/BlockArchitectDay.tsx
@@ -1,26 +1,35 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import Card from 'react-bootstrap/Card';
 import { Button, Row, Col, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const BlockArchitecDay = (props:any) => {
   return (
-    <Fragment>
-      {/* <a href={'authors/' + props.author.id} className="Author-reference"></a> */}
+    <Fragment>      
       <Card bg="light" text="dark" className="m-3" >
-        <Card.Header as="h5" className="text-center">Архитектор дня</Card.Header>
+        <Card.Header as="h5" className="text-center">
+          Архитектор дня
+        </Card.Header>
         <Card.Body>
           <Row>
-            <Col md={3}>              
-              <Image src={props.architect.imageSrc} thumbnail />
+            <Col xl={3} lg={4} md={5}>              
+              <Image 
+                src={props.architect.imageSrc}                 
+                thumbnail 
+              />
             </Col>
-            <Col md={9}>
-              <Card.Title>{props.architect.name}</Card.Title>
-              <Card.Text>{props.architect.description}</Card.Text>          
-              <Button variant="dark">Перейти</Button>
+            <Col xl={9} lg={8} md={7}>
+              <Card.Title className="mt-2">
+                {props.architect.name}
+              </Card.Title>
+              <Card.Text>{props.architect.description}</Card.Text>                        
+              <Button variant="dark">
+                <Link to={`/authors/${props.architect.id}`}>Перейти</Link>
+              </Button>
             </Col>
           </Row>          
         </Card.Body>
-      </Card>
+      </Card>      
     </Fragment>
   );
 };
