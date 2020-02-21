854142cab3212fcab4ec394bf01894006f729928
Ilya Ivanchikov <phantomonster1@mail.ru>
2020-02-21 22:39:16 +0200

    Revert "feat: add new lib and change Link path"
    This reverts commit ee344ca550bf7368dedbc1545f571e7aa4adac90.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import routes from 'constants/routes';
-import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <Navbar bg="light" expand="lg" variant="light">
-      <LinkContainer to={routes.LANDING}>
-      <Navbar.Brand>
+      <Navbar.Brand href={routes.LANDING}>
         <img
           alt=""
           src="/images/brand.png"
@@ -17,19 +14,12 @@ const Header = () => {
           className="d-inline-block align-top"
         />{' '}
       </Navbar.Brand>
-      </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <LinkContainer to={routes.LANDING}>
-            <Nav.Link>Главная</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to={routes.AUTHORS}>
-            <Nav.Link>Авторы</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to={routes.AUTHOR}>
-            <Nav.Link>Автор</Nav.Link>
-          </LinkContainer>
+          <Nav.Link href={routes.LANDING}>Главная</Nav.Link>
+          <Nav.Link href={routes.AUTHORS}>Авторы</Nav.Link>
+          <Nav.Link href={routes.AUTHOR}>Автор</Nav.Link>
         </Nav>
         <Nav className="mr-2">
           <NavDropdown title="Выберите язык" id="basic-nav-dropdown">
