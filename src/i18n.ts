21939da57685ef4d1d729a996e34363a68a97f33
Olga Ivanova <olgai1409@gmail.com>
2020-02-25 14:18:45 +0500

    feat: add translation for headers

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,10 +3,12 @@ import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Cache from 'i18next-localstorage-cache';
 import lang from './constants/languages';
 
 i18n
   .use(Backend)
+  .use(Cache)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
