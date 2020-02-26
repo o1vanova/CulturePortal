35e48c1f73d619836da3d84384187b3972972607
Ildar107 <ildar107wk@gmail.com>
2020-02-27 02:01:09 +0300

    refactor: change mapgl to react-mapgl

--- src/components/author/blockAuthorLocation/BlockAuthorLocation.tsx
+++ src/components/author/blockAuthorLocation/BlockAuthorLocation.tsx
@@ -1,34 +1,56 @@
 import React, { Component } from 'react';
-import mapboxgl from 'mapbox-gl';
+import ReactMapGL, { Marker, setRTLTextPlugin } from "react-map-gl";
 import './BlockAuthorLocation.scss';
 
-class BlockAuthorLocation extends Component {
-    constructor(props) {
-        super(props);
+type MapState = {
+    viewport : {
+        width: string,
+        height: string,
+        latitude: number,
+        longitude: number,
+        zoom: number
     }
-
-    mapContainer: any;
-
-    componentDidMount() {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiaWxkYXIxMDciLCJhIjoiY2s2dG9la25vMDE3YjNsazNhOWV0NzUyYiJ9.PZ4hkyRPbL0fCc1YURD6wg';
-        const initMap = {
-            lng: 27.9533882,
-            lat: 53.7098083,
-            zoom: 8
-        };
-        const map = new mapboxgl.Map({
-        container: this.mapContainer,
-        style: 'mapbox://styles/ildar107/ck6w6c31f0fah1ipl0crxsqo6',
-        center: [initMap.lng, initMap.lat],
-        zoom: initMap.zoom
-        });
+    userLocation : {
+      latitude: number,
+      longitude: number
     }
+};
 
-    render() {
-        return (<div>
-            <div ref={el => this.mapContainer = el} className='map__container'></div>
-        </div>);
-    }
+class BlockAuthorLocation extends Component<{}, MapState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        width: "100%",
+        height: "100%",
+        latitude: 53.7098083,
+        longitude: 27.9533882,
+        zoom: 9
+      },
+      userLocation: {
+        latitude: 53.7098083,
+        longitude: 27.9533882,
+      }
+    };
+  }
+
+  render() {
+    return (<div  className='map__container'>
+      <ReactMapGL 
+        {...this.state.viewport} 
+        onViewportChange={(viewport => this.setState({viewport}))}
+        mapboxApiAccessToken='pk.eyJ1IjoiaWxkYXIxMDciLCJhIjoiY2s2dG9la25vMDE3YjNsazNhOWV0NzUyYiJ9.PZ4hkyRPbL0fCc1YURD6wg' 
+        mapStyle='mapbox://styles/ildar107/ck6w6c31f0fah1ipl0crxsqo6'
+        mapboxLanguage ="ru">
+        
+        <Marker
+          latitude={this.state.userLocation.latitude}
+          longitude={this.state.userLocation.longitude}>
+        <img src='/images/pin.svg' className="marker__image" />
+        </Marker>
+      </ReactMapGL>
+    </div>);
+  }
 }
 
 export default BlockAuthorLocation;
