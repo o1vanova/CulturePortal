0d35ca5a3e39a16b6f85e68ed649c052561c2119
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2020-02-28 19:48:26 +0300

    feat: add component Wrapper

--- src/pages/styleGuide/styleGuide.tsx
+++ src/pages/styleGuide/styleGuide.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Colors } from '../../components/style/colors/colors';
+import Wrapper from '../../components/style/wrapper/Wrapper';
 
-const MainPage = (): JSX.Element => {
+
+const StylePage = (): JSX.Element => {
   return (
     <Container className="page-styleGuide">
+      <h2 className="mt-3 text-center">Our styleGuide</h2>        
       <Row>
         <Col>
-            <h2 className="mt-3 text-center">Our styleGuide</h2>
-        </Col>
-        <Col>
-            <Colors />  
+          <Wrapper title="Button" code="<></>">
+            <Colors />
+          </Wrapper>            
         </Col>
       </Row>
     </Container>
   );
 };
 
-export default MainPage;
+export default StylePage;
