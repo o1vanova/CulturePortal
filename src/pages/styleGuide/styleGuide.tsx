4e1e520c2cfbb97b9ea91916fedc715257f09476
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2020-02-29 17:01:40 +0300

    feat: add code example file for style guide

--- src/pages/styleGuide/styleGuide.tsx
+++ src/pages/styleGuide/styleGuide.tsx
@@ -8,6 +8,7 @@ import { Inputs } from '../../components/style/inputs/Inputs';
 import { Tables } from '../../components/style/tables/Tables';
 import Wrapper from '../../components/style/wrapper/Wrapper';
 import Cards from '../../components/style/cards/Cards';
+import codeExample from '../../constants/codeExample'
 
 import './StyleGuide.scss';
 
@@ -15,28 +16,25 @@ const StylePage = (): JSX.Element => {
   return (
     <Container className="page-styleGuide">
       <h2 className="mt-3 text-center">Our styleGuide</h2>
-      <Wrapper title="Color" codeExample="<></>">
+      <Wrapper title="Color" codeExample={codeExample.Colors}>
         <Colors />
       </Wrapper>
-      <Wrapper title="Typography" codeExample="<></>">
+      <Wrapper title="Typography" codeExample={codeExample.Typography}>
         <Typography />
       </Wrapper>
-      <Wrapper
-        title="Button"
-        codeExample={`<Button>Secondary</Button>\n\t<Button>Secondary</Button>\n<Button>Secondary</Button>`}
-      >
+      <Wrapper title="Button" codeExample={codeExample.Buttons}>
         <Buttons />
       </Wrapper>
-      <Wrapper title="Inputs" codeExample="<></>">
+      <Wrapper title="Inputs" codeExample={codeExample.Inputs}>
         <Inputs />
       </Wrapper>
-      <Wrapper title="CheckBox" codeExample="<></>">
+      <Wrapper title="CheckBox" codeExample={codeExample.CheckBox}>
         <CheckBoxs />
       </Wrapper>
-      <Wrapper title="Tables" codeExample="<></>">
+      <Wrapper title="Table" codeExample={codeExample.Tables}>
         <Tables />
       </Wrapper>
-      <Wrapper title="Cards" codeExample="<></>">
+      <Wrapper title="Cards" codeExample={codeExample.Cards}>
         <Cards />
       </Wrapper>
     </Container>
